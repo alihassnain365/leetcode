042d586197ebd80ddbf1443c9612b20dd34f32a8
Ali Hassnain <ali.hassnain0365@gmail.com>
2026-03-13 12:58:40 +0500

    Add twoSum function in Two_Sum.cpp
    Implement twoSum function to find indices of two numbers that add up to a target.

---
+++ Easy/Two_Sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i<nums.size(); i++)
+        {
+            
+            
+                for (int k = i+1; k<nums.size(); k++)
+                {
+                    
+                    
+                        if (nums[i] + nums[k] == target)
+                        {
+                            return {i,k};
+                        }
+                    
+                }
+            
+        }
+        return {0,1};
+        
+    }
+};
